bd355f657ccd256d4c349cc703f95009a05b08f8
Oleg <prog979@yandex.ru>
2021-07-07 15:19:55 +0300

    The final commit of a fully completed application

---
+++ src/main/java/org/springboot/example/repository/AnswerRepository.java
@@ -0,0 +1,11 @@
+package org.springboot.example.repository;
+
+import org.springboot.example.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
+}
---
+++ src/main/java/org/springboot/example/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package org.springboot.example.repository;
+
+import org.springboot.example.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+}
